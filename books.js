a6d428f53a0480e939d4fe593041d1a32137e3bd
nicoleSclark99 <nicole.scionti99@gamil.com>
2026-06-19 14:58:42 -0400

    added js for loading icon

--- books.js
+++ books.js
@@ -1,8 +1,16 @@
-function renderBooks(filter) {
+let books;
+
+async function renderBooks(filter) {
   const booksWrapper = document.querySelector('.books');
 
-  const books = getBooks();
-
+  booksWrapper.classList += ' books__loading'
+  
+  if (!books){
+    books = await getBooks();
+  }
+  
+  
+  booksWrapper.classList.remove('books__loading')
   
   if (filter === 'LOW_TO_HIGH') {
     books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice))
@@ -73,7 +81,9 @@ setTimeout( () => {
 
 // FAKE DATA
 function getBooks() {
-      return [
+  return new Promise ((resolve) => {
+    setTimeout( () => {
+    resolve ([
         {
           id: 1,
           title: "Crack the Coding Interview",
@@ -162,4 +172,7 @@ function getBooks() {
           salePrice: null,
           rating: 4.5,
         }
-      ]}
+      ])  
+    }, 1000)
+  })
+}
